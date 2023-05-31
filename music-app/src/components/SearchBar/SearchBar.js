c0e14f6107f9b3b429c5e013d06a9639f3772c79
Aayush Kachhwaha <aayushk2609@gmail.com>
2023-06-01 00:06:30 +0530

    update

--- music-app/src/components/SearchBar/SearchBar.js
+++ music-app/src/components/SearchBar/SearchBar.js
@@ -1,4 +1,37 @@
 import React from "react";
 import"./SearchBar.css";
 
-class SerachBar
+class SerachBar extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            term:""
+        };
+        this.handleTermChange= this.handleTermChange.bind(this);
+        this.serach=this.serach.bind(this);
+        this.handleEnter= this.handleEnter.bind(this);
+    }
+
+    handleTermChange(event){
+        this.setState({term: event.target.value});
+    }
+
+    serach(){
+        this.props.onSearch(this.state.term);
+    }
+
+    handleEnter(event){
+        if(event.keyCode === 13){
+            this.serach();
+        }
+    }
+
+    render(){
+        return{
+            <div className = "SearchBar">
+            <><input placeholder="Enter song, album or artist" onChange={this.handleTermChange} onKeyUp={this.handleEnter} /><button className="SerachButton" onClick={this.serach}></button></>
+            </div>
+        };
+    }
+}
