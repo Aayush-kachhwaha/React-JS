17e7f826c75494bb31b70a9e00438b73592cbd33
Aayush Kachhwaha <aayushk2609@gmail.com>
2023-06-02 10:32:15 +0530

    final update

--- First project/calculator/src/App.js
+++ First project/calculator/src/App.js
@@ -2,15 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import Layout from './components/layout';
 
-// npm start to run the app
+
 
 function App() {
   return (
     <div className = "App">
-      <Layout></Layout>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-// Ctrl + C to stop the app
+
 
 export default App;
