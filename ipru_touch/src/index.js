3b171df02d41ca7131d510ab4b96ab1753ec410b
Aayush Kachhwaha <aayushk2609@gmail.com>
2023-07-15 08:56:45 +0530

    Update

---
+++ ipru_touch/src/index.js
@@ -0,0 +1,11 @@
+
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import nev from './Componen/Nev bar/nev';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  nev
+);
+
